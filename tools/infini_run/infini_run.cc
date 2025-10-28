69c7097d42dcbf27afdc4a5cbe18067dced20d2a
kilinchange <kilinchange@163.com>
2025-10-28 12:53:07 +0000

    feat: use shared file to distribute ncclUniqueId in infini_run

--- tools/infini_run/infini_run.cc
+++ tools/infini_run/infini_run.cc
@@ -1,8 +1,17 @@
+#include <cstdlib>
+#include <fstream>
+#include <ios>
+#include <sstream>
 #include <string>
 #include <sys/wait.h>
-#include <vector>
-#include <cstdlib>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <vector>
+#include <cstdio>
+
+#ifdef USE_NCCL
+#include <nccl.h>
+#endif
 
 #include "gflags/gflags.h"
 #include "glog/logging.h"
@@ -12,22 +21,47 @@ DEFINE_int32(nproc_per_node, 1, "Number of processes per node");
 DEFINE_int32(node_rank, 0, "Rank of this node");
 DEFINE_string(rdzv_endpoint, "127.0.0.1:29500", "Rendezvous endpoint (host:port)");
 
-int main(int argc, char** argv) {
+#ifdef USE_NCCL
+std::string NcclIdToString(const ncclUniqueId& id) {
+    std::ostringstream oss;
+    for (int i = 0; i < NCCL_UNIQUE_ID_BYTES; ++i) {
+        oss << std::hex << std::uppercase << (int)(unsigned char)id.internal[i];
+    }
+    return oss.str();
+}
+#endif
+
+int main(int argc, char **argv) {
     gflags::ParseCommandLineFlags(&argc, &argv, true);
     google::InitGoogleLogging(argv[0]);
 
     CHECK_GE(argc, 2) << "No training prgram specified!";
 
     std::string train_program = argv[1];
-    std::vector<char*> train_argv;
-    for (int i = 1; i < argc; ++i) {
-        train_argv.push_back(argv[i]);
-    }
+    std::vector<char *> train_argv;
+    for (int i = 1; i < argc; ++i) { train_argv.push_back(argv[i]); }
     train_argv.push_back(nullptr);
 
     int world_size = FLAGS_nnodes * FLAGS_nproc_per_node;
     std::string master_addr = FLAGS_rdzv_endpoint.substr(0, FLAGS_rdzv_endpoint.find(':'));
     std::string master_port = FLAGS_rdzv_endpoint.substr(FLAGS_rdzv_endpoint.find(':') + 1);
+
+    const char* nccl_id_path = "/data/shared/InfiniTrain-dev/data/nccl_id.bin";
+    const char* nccl_id_tmp_path = "/data/shared/InfiniTrain-dev/data/nccl_id.tmp";
+
+#ifdef USE_NCCL
+    if (FLAGS_node_rank == 0) {
+        ncclUniqueId id;
+        ncclGetUniqueId(&id);
+
+        std::ofstream ofs(nccl_id_tmp_path, std::ios::binary);
+        ofs.write((char *)&id, sizeof(id));
+        ofs.close();
+
+        // atomic operation
+        rename(nccl_id_tmp_path, nccl_id_path);
+    }
+#endif
 
     for (int local_proc_rank = 0; local_proc_rank < FLAGS_nproc_per_node; ++local_proc_rank) {
         pid_t pid = fork();
@@ -39,6 +73,20 @@ int main(int argc, char** argv) {
             setenv("NPROC_PER_NODE", std::to_string(FLAGS_nproc_per_node).c_str(), 1);
             setenv("MASTER_ADDR", master_addr.c_str(), 1);
             setenv("MASTER_PORT", master_port.c_str(), 1);
+
+#ifdef USE_NCCL
+            struct stat st;
+            while (stat(nccl_id_path, &st) != 0) {
+                usleep(1000);
+            }
+
+            ncclUniqueId id;
+            std::ifstream ifs(nccl_id_path, std::ios::binary);
+            ifs.read((char*)&id, sizeof(id));
+
+            std::string id_str = NcclIdToString(id);
+            setenv("NCCL_UNIQUE_ID", id_str.c_str(), 1);
+#endif
 
             execvp(train_program.c_str(), train_argv.data());
             perror("exec failed");
