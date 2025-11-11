890e683ac1cbab1d511bfb745ea9669b69f8b710
kilinchange <kilinchange@163.com>
2025-11-11 08:41:37 +0000

    refactor: refactor the naming of ncclUniqueId files and add a unified cleanup logic

--- tools/infini_run/infini_run.cc
+++ tools/infini_run/infini_run.cc
@@ -1,13 +1,11 @@
+#include <cstdio>
 #include <cstdlib>
-#include <fstream>
-#include <ios>
-#include <sstream>
+#include <filesystem>
+#include <regex>
 #include <string>
 #include <sys/wait.h>
-#include <sys/stat.h>
 #include <unistd.h>
 #include <vector>
-#include <cstdio>
 
 #ifdef USE_NCCL
 #include <nccl.h>
@@ -21,15 +19,19 @@ DEFINE_int32(nproc_per_node, 1, "Number of processes per node");
 DEFINE_int32(node_rank, 0, "Rank of this node");
 DEFINE_string(rdzv_endpoint, "127.0.0.1:29500", "Rendezvous endpoint (host:port)");
 
-#ifdef USE_NCCL
-std::string NcclIdToString(const ncclUniqueId& id) {
-    std::ostringstream oss;
-    for (int i = 0; i < NCCL_UNIQUE_ID_BYTES; ++i) {
-        oss << std::hex << std::uppercase << std::setw(2) << std::setfill('0') << (int)(unsigned char)id.internal[i];
+void CleanupNcclIdFiles() {
+    const std::filesystem::path cwd = std::filesystem::current_path();
+    std::regex pattern(R"(ncclUniqueId_.*\.bin)");
+
+    for (const auto &entry : std::filesystem::directory_iterator(cwd)) {
+        if (entry.is_regular_file()) {
+            const std::string filename = entry.path().filename().string();
+            if (std::regex_match(filename, pattern)) {
+                std::filesystem::remove(entry.path());
+            }
+        }
     }
-    return oss.str();
 }
-#endif
 
 int main(int argc, char **argv) {
     gflags::ParseCommandLineFlags(&argc, &argv, true);
@@ -46,23 +48,6 @@ int main(int argc, char **argv) {
     std::string master_addr = FLAGS_rdzv_endpoint.substr(0, FLAGS_rdzv_endpoint.find(':'));
     std::string master_port = FLAGS_rdzv_endpoint.substr(FLAGS_rdzv_endpoint.find(':') + 1);
 
-    const char* nccl_id_path = "/data/shared/InfiniTrain-dev/data/nccl_id.bin";
-    const char* nccl_id_tmp_path = "/data/shared/InfiniTrain-dev/data/nccl_id.tmp";
-
-#ifdef USE_NCCL
-    if (FLAGS_node_rank == 0) {
-        ncclUniqueId id;
-        ncclGetUniqueId(&id);
-
-        std::ofstream ofs(nccl_id_tmp_path, std::ios::binary);
-        ofs.write((char *)&id, sizeof(id));
-        ofs.close();
-
-        // atomic operation
-        rename(nccl_id_tmp_path, nccl_id_path);
-    }
-#endif
-
     for (int local_proc_rank = 0; local_proc_rank < FLAGS_nproc_per_node; ++local_proc_rank) {
         pid_t pid = fork();
         if (pid == 0) {
@@ -73,20 +58,6 @@ int main(int argc, char **argv) {
             setenv("NPROC_PER_NODE", std::to_string(FLAGS_nproc_per_node).c_str(), 1);
             setenv("MASTER_ADDR", master_addr.c_str(), 1);
             setenv("MASTER_PORT", master_port.c_str(), 1);
-
-#ifdef USE_NCCL
-            struct stat st;
-            while (stat(nccl_id_path, &st) != 0) {
-                usleep(1000);
-            }
-
-            ncclUniqueId id;
-            std::ifstream ifs(nccl_id_path, std::ios::binary);
-            ifs.read((char*)&id, sizeof(id));
-
-            std::string id_str = NcclIdToString(id);
-            setenv("NCCL_UNIQUE_ID", id_str.c_str(), 1);
-#endif
 
             execvp(train_program.c_str(), train_argv.data());
             perror("exec failed");
@@ -101,7 +72,7 @@ int main(int argc, char **argv) {
 
 #ifdef USE_NCCL
     if (FLAGS_node_rank == 0) {
-        std::remove(nccl_id_path);
+        CleanupNcclIdFiles();
     }
 #endif
 
