1ddcbb65c0d6f52b9b87e55125a51922d9026a77
cx <cx2016013@163.com>
2026-08-27 05:35:22 +0000

    fix: address backend-independent correctness issues
    - honor DDP reduction configuration and validate process group devices
    - fix elementwise and CUDA linear backward correctness
    - remove the unsupported distributed optimizer test
    - clean up related build diagnostics and test documentation

--- tests/optimizer/test_optimizer_parameter_names.cc
+++ tests/optimizer/test_optimizer_parameter_names.cc
@@ -3,14 +3,6 @@
 
 #include "gtest/gtest.h"
 
-#include "infini_train/include/nn/modules/linear.h"
-#include "infini_train/include/nn/parallel/ddp/distributed_data_parallel.h"
-#include "infini_train/include/nn/parallel/ddp/distributed_data_parallel_config.h"
-#include "infini_train/include/nn/parallel/ddp/distributed_optimizer.h"
-#include "infini_train/include/nn/parallel/global.h"
-#include "infini_train/include/nn/parallel/process_group.h"
-#include "infini_train/include/nn/parallel/rank.h"
-#include "infini_train/include/nn/parallel/utils.h"
 #include "infini_train/include/optimizer.h"
 #include "infini_train/include/tensor.h"
 
@@ -50,38 +42,6 @@ TEST_P(OptimizerParameterNamesTest, ConstructorMatchesNamesToOptimizerParameterO
     EXPECT_TRUE(state.contains("adam.v.second"));
     EXPECT_TRUE(state.contains("adam.m.first"));
     EXPECT_TRUE(state.contains("adam.v.first"));
-}
-
-TEST_P(OptimizerParameterNamesTest, DistributedOptimizerPropagatesNamesToShardOptimizer) {
-    ONLY_CUDA();
-    REQUIRE_MIN_DEVICES(2);
-    if (nn::parallel::global::GetDataParallelSize() != 2) {
-        GTEST_SKIP() << "requires PROC_WORLD_SIZE=2";
-    }
-
-    const nn::parallel::Rank rank(/*process_rank=*/0, /*thread_rank=*/0, /*process_size=*/1, /*thread_size=*/2);
-    auto *pg_factory = nn::parallel::ProcessGroupFactory::Instance(Device::DeviceType::kCUDA);
-    pg_factory->GetOrCreate(nn::parallel::GetDataParallelProcessGroupName(rank.GlobalRank()),
-                            nn::parallel::GetDataParallelGroupRanks(rank.GlobalRank()));
-
-    auto model = std::make_shared<nn::Linear>(4, 4, /*bias=*/false, GetDevice());
-    const auto named_parameters = model->NamedParameters();
-
-    nn::parallel::DistributedDataParallelConfig ddp_config;
-    ddp_config.zero_stage = 1;
-    ddp_config.overlap_grad_reduce = false;
-    ddp_config.overlap_param_gather = false;
-    auto ddp_model = std::make_shared<nn::parallel::DistributedDataParallel>(model, rank, ddp_config);
-
-    nn::parallel::DistributedOptimizer optimizer(optimizers::Adam::CreateNamed(0.001), named_parameters,
-                                                 std::vector<std::shared_ptr<nn::Module>>{ddp_model},
-                                                 /*ddp_world_size=*/2, /*ddp_rank=*/0);
-    const auto state = optimizer.StateDict();
-
-    EXPECT_TRUE(state.contains("adam.m.weight"));
-    EXPECT_TRUE(state.contains("adam.v.weight"));
-    EXPECT_FALSE(state.contains("adam.m.0"));
-    EXPECT_FALSE(state.contains("adam.v.0"));
 }
 
 TEST_P(OptimizerParameterNamesTest, PreservesNumericKeysWhenNamesAreNotSet) {
