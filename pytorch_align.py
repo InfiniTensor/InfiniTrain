a17d3e6ab1b67f19652110bb29205467617570fe
ChenYuxin-meow <c17835905226@163.com>
2026-09-19 18:50:17 +0800

    chore: remove pytorch_align.py from PR

--- pytorch_align.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn as nn
-
-# 固定输入和权重
-x = torch.arange(25, dtype=torch.float32).reshape(1, 1, 5, 5)
-w = torch.ones(1, 1, 3, 3)
-y = torch.nn.functional.conv2d(x, w)
-print("PyTorch Forward:", y)
